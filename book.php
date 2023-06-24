8562c0ce5d47369df3b21482b1847f2516fd4dcd
Luke Edmunds <Luke.edmunds49@ed.can.nl.ca>
2023-06-23 23:58:29 -0230

    learned about classes and objects in php

--- book.php
+++ book.php
@@ -1,3 +1,98 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>A quick test</title>
+  </head>
+  <body>
+  <?php
 echo "hello world";
-?> 
+?>
+<?php // test1.php
+  $username = "Fred Smith";
+  echo $username;
+  echo "<br>";
+  $current_user = $username;
+  echo $current_user;
+  //creating a object $object = new User;
+$temp   = new User('name', 'password');
+
+?>
+
+<?php
+//accessing objects
+  $object = new User;
+  print_r($object); echo "<br>";
+
+  $object->name     = "Joe";
+  $object->password = "mypass";
+  print_r($object); echo "<br>";
+
+  $object->save_user();
+
+  class User
+  {
+    public $name, $password;
+
+    function save_user()
+    {
+      echo "Save User code goes here";
+    }
+  }
+?>
+<?php
+//cloning an object
+//not using the clone in the second method so it will be just AMY AMy
+  $object1       = new User();
+  $object1->name = "Alice";
+  $object2       = $object1;
+  $object2->name = "Amy";
+
+  echo "object1 name = " . $object1->name . "<br>";
+  echo "object2 name = " . $object2->name;
+
+  class User
+  {
+    public $name;
+  }
+?>
+
+<?php
+  $object1       = new User();
+  $object1->name = "Alice";
+  $object2       = clone $object1;
+  $object2->name = "Amy";
+
+  echo "object1 name = " . $object1->name . "<br>";
+  echo "object2 name = " . $object2->name;
+
+  class User
+  {
+    public $name;
+  }
+?>
+
+<?php
+//methods
+  class User
+  {
+    public $name, $password;
+
+    function get_password()
+    {
+      return $this->password;
+    }
+  }
+?>
+
+<?php
+// numerically indexed arrays
+  $paper[] = "Copier";
+  $paper[] = "Inkjet";
+  $paper[] = "Laser";
+  $paper[] = "Photo";
+
+  print_r($paper);
+?>
+
+  </body>
+</html>
