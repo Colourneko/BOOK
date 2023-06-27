49411aeb4d753991ac2f16db916088e3eb633ff4
Luke Edmunds <Luke.edmunds49@ed.can.nl.ca>
2023-06-27 17:02:11 -0230

    learning print f to reading and writing to a file

--- book.php
+++ book.php
@@ -93,6 +93,162 @@ $temp   = new User('name', 'password');
 
   print_r($paper);
 ?>
+<?php
+printf("There are %d items in your basket", 3);
+printf("My name is %s. I'm %d years old, which is %X in hexadecimal",
+  'Simon', 33, 33);
+  printf("<span style='color:#%X%X%X'>Hello</span>", 65, 127, 245);
+  //Precision Setting
+  printf("The result is: $%.2f", 123.42 / 12);
+?>
+<?php
+  echo "<pre>"; // Enables viewing of the spaces
 
+  // Pad to 15 spaces
+  printf("The result is $%15f\n", 123.42 / 12);
+
+  // Pad to 15 spaces, fill with zeros
+  printf("The result is $%015f\n", 123.42 / 12);
+
+  // Pad to 15 spaces, 2 decimal places precision
+  printf("The result is $%15.2f\n", 123.42 / 12);
+
+  // Pad to 15 spaces, 2 decimal places precision, fill with zeros
+  printf("The result is $%015.2f\n", 123.42 / 12);
+
+  // Pad to 15 spaces, 2 decimal places precision, fill with # symbol
+  printf("The result is $%'#15.2f\n", 123.42 / 12);
+  /* The result is $      10.285000
+The result is $00000010.285000
+The result is $          10.29
+The result is $000000000010.29
+The result is $##########10.29 */
+?>
+<?php
+  echo "<pre>"; // Enables viewing of the spaces
+
+  $h = 'Rasmus';
+
+  printf("[%s]\n",         $h); // Standard string output
+  printf("[%12s]\n",       $h); // Right justify with spaces to width 12
+  printf("[%-12s]\n",      $h); // Left justify with spaces
+  printf("[%012s]\n",      $h); // Pad with zeros
+  printf("[%'#12s]\n\n",   $h); // Use the custom padding character '#'
+
+  $d = 'Rasmus Lerdorf';        // The original creator of PHP
+
+  printf("[%12.8s]\n",     $d); // Right justify, cutoff of 8 characters
+  printf("[%-12.12s]\n",   $d); // Left justify, cutoff of 12 characters
+  printf("[%-'@12.10s]\n", $d); // Left justify, pad with '@', cutoff 10 chars
+  /*
+  [Rasmus]
+[      Rasmus]
+[Rasmus      ]
+[000000Rasmus]
+[######Rasmus]
+
+[    Rasmus L]
+[Rasmus Lerdo]
+[Rasmus Ler@@]&*/
+?>
+<?php // testfile.php
+//creating a file
+  $fh = fopen("testfile.txt", 'w') or die("Failed to create file");
+
+  $text = <<<_END
+Line 1
+Line 2
+Line 3
+_END;
+
+  fwrite($fh, $text) or die("Could not write to file");
+  fclose($fh);
+  echo "File 'testfile.txt' written successfully";
+?>
+
+<?php
+// reading a file
+  $fh = fopen("testfile.txt", 'r') or
+    die("File does not exist or you lack permission to open it");
+
+  $line = fgets($fh);
+  fclose($fh);
+  echo $line;
+?>
+<?php
+//using fread
+  $fh = fopen("testfile.txt", 'r') or
+    die("File does not exist or you lack permission to open it");
+
+  $text = fread($fh, 3);
+  fclose($fh);
+  echo $text;
+  ?>
+  <?php // copyfile2.php
+  //using the NOT function
+  if (!copy('testfile.txt', 'testfile2.txt')) echo "Could not copy file";
+  else echo "File successfully copied to 'testfile2.txt'";
+?>
+<?php // movefile.php
+// moving a file
+  if (!rename('testfile2.txt', 'testfile2.new'))
+    echo "Could not rename file";
+  else echo "File successfully renamed to 'testfile2.new'";
+?>
+<?php // deletefile.php
+// deleting a file
+  if (!unlink('testfile2.new')) echo "Could not delete file";
+  else echo "File 'testfile2.new' successfully deleted";
+?>
+<?php // update.php
+//updating a file
+  $fh   = fopen("testfile.txt", 'r+') or die("Failed to open file");
+  $text = fgets($fh);
+
+  fseek($fh, 0, SEEK_END);
+  fwrite($fh, "\n$text") or die("Could not write to file");
+  fclose($fh);
+
+  echo "File 'testfile.txt' successfully updated";
+?>
+<?php
+//locking a file witg LOCK_EX
+  $fh   = fopen("testfile.txt", 'r+') or die("Failed to open file");
+  $text = fgets($fh);
+
+  if (flock($fh, LOCK_EX))
+  {
+    fseek($fh, 0, SEEK_END);
+    fwrite($fh, "$text") or die("Could not write to file");
+    flock($fh, LOCK_UN);
+  }
+
+  fclose($fh);
+  echo "File 'testfile.txt' successfully updated";
+?>
+<?php
+//reading the whole file
+  echo "<pre>";  // Enables display of line feeds
+  echo file_get_contents("testfile.txt");
+  echo "</pre>"; // Terminates <pre> tag
+?>
+<?php // upload.php
+  echo <<<_END
+    <html><head><title>PHP Form Upload</title></head><body>
+    <form method='post' action='upload.php' enctype='multipart/form-data'>
+    Select File: <input type='file' name='filename' size='10'>
+    <input type='submit' value='Upload'>
+    </form>
+_END;
+
+  if ($_FILES)
+  {
+    $name = $_FILES['filename']['name'];
+    move_uploaded_file($_FILES['filename']['tmp_name'], $name);
+    echo "Uploaded image '$name'<br><img src='$name'>";
+  }
+
+  echo "</body></html>";
+?>
   </body>
 </html>
